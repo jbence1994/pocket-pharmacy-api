ef4aa9ab976b9855aadcfd29f48e5ed9316f408b
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-04-23 17:56:11 +0200

    Rename main class

--- src/main/kotlin/com/pocketpharmacy/api/Application.kt
+++ src/main/kotlin/com/pocketpharmacy/api/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PocketPharmacyApiApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<PocketPharmacyApiApplication>(*args)
+    runApplication<Application>(*args)
 }
