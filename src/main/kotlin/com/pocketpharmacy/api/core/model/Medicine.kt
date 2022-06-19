e963300545d62ca084a63927322a5bee7a6c63a7
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-06-19 10:04:30 +0200

    Specify table names

--- src/main/kotlin/com/pocketpharmacy/api/core/model/Medicine.kt
+++ src/main/kotlin/com/pocketpharmacy/api/core/model/Medicine.kt
@@ -6,8 +6,10 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.OneToOne
+import javax.persistence.Table
 
 @Entity
+@Table(name = "medicines")
 data class Medicine(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
