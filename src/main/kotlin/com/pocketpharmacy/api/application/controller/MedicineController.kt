8518c4262738f67095feefe08fa3f73c4a1744ca
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-07-09 19:05:57 +0200

    Clean up mock responses

--- src/main/kotlin/com/pocketpharmacy/api/application/controller/MedicineController.kt
+++ src/main/kotlin/com/pocketpharmacy/api/application/controller/MedicineController.kt
@@ -3,63 +3,24 @@ package com.pocketpharmacy.api.application.controller
 import com.pocketpharmacy.api.application.constants.RouteConstants.ID
 import com.pocketpharmacy.api.application.constants.RouteConstants.MEDICINES
 import com.pocketpharmacy.api.application.resource.response.MedicineResponse
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
 
 @RestController
 @RequestMapping(MEDICINES)
 class MedicineController {
 
-    @Suppress("MagicNumber")
-    private val medicine1: MedicineResponse = MedicineResponse(
-        id = 1,
-        name = "Xanax",
-        description = "Domestic painkiller.",
-        amount = 20.0,
-        dailyDosageAmount = 1.5,
-        unit = "mg",
-        expirationDate = LocalDate.of(2023, 1, 1),
-        needPrescription = false,
-        quantity = 20
-    )
-
-    @Suppress("MagicNumber")
-    private val medicine2: MedicineResponse = MedicineResponse(
-        id = 2,
-        name = "Rubophen",
-        description = "For high fever.",
-        amount = 20.0,
-        dailyDosageAmount = 2.0,
-        unit = "mg",
-        expirationDate = LocalDate.of(2024, 1, 1),
-        needPrescription = true,
-        quantity = 20
-    )
-
     @GetMapping
     fun getMedicines(): ResponseEntity<List<MedicineResponse>> {
-        val medicines: List<MedicineResponse> = listOf(medicine1, medicine2)
-        return ResponseEntity(medicines, HttpStatus.OK)
+        throw NotImplementedError("Operation not yet implemented.")
     }
 
-    @Suppress("MagicNumber")
     @GetMapping(ID)
     fun getMedicine(@PathVariable id: Int): ResponseEntity<MedicineResponse> {
-        return when (id) {
-            1 -> {
-                ResponseEntity(medicine1, HttpStatus.OK)
-            }
-            2 -> {
-                ResponseEntity(medicine2, HttpStatus.OK)
-            }
-            else -> {
-                ResponseEntity(HttpStatus.NOT_FOUND)
-            }
-        }
+        throw NotImplementedError("Operation not yet implemented.")
     }
 }
