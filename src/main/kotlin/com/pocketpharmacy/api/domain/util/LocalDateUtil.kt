58120bb57a81dc60cae4bff221901a85cfcfaa53
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-07-10 10:59:16 +0200

    Move medicine utils test to proper package and remove local date utils

--- src/main/kotlin/com/pocketpharmacy/api/domain/util/LocalDateUtil.kt
+++
@@ -1,5 +0,0 @@
-package com.pocketpharmacy.api.domain.util
-
-import java.time.LocalDate
-
-fun getLocalDateNow(): LocalDate = LocalDate.now()
--- src/main/kotlin/com/pocketpharmacy/api/domain/util/MedicineUtils.kt
+++ src/main/kotlin/com/pocketpharmacy/api/domain/util/MedicineUtils.kt
@@ -1,5 +1,6 @@
 package com.pocketpharmacy.api.domain.util
 
 import com.pocketpharmacy.api.data.model.Medicine
+import java.time.LocalDate
 
-fun Medicine.isExpired(): Boolean = getLocalDateNow() > expirationDate
+fun Medicine.isExpired(): Boolean = LocalDate.now() > expirationDate
